4b5b9047357316d4c7e436692a149463714c77dc
Wisnu Prasojo Widianto <wisnuprsj@gmail.com>
2021-11-20 01:18:24 +0700

    added some changes section 4

--- Section-2/posts/index.js
+++ Section-2/posts/index.js
@@ -32,7 +32,7 @@ app.post("/posts", async (req, res) => {
     title,
   };
 
-  await axios.post("http://localhost:4005/events", {
+  await axios.post("http://event-bus-srv:4005/events", {
     type: "PostCreated",
     data: {
       id,
@@ -50,5 +50,6 @@ app.post("/events", (req, res) => {
 });
 
 app.listen(4000, () => {
+  console.log("v55");
   console.log(`this server is running on port 4000`);
 });
