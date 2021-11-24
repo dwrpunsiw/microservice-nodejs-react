6546830c230b2e3c0c327df967731379cd60a525
Wisnu Prasojo Widianto <wisnuprsj@gmail.com>
2021-11-25 00:55:31 +0700

    finished appendix-b

---
+++ Appendix-B/maps/src/index.ts
@@ -0,0 +1,15 @@
+/// <reference types="@types/google.maps" />
+
+import { CustomMap } from "./CustomMap";
+import { User } from "./User";
+import { Company } from "./Company";
+
+const customMap = new CustomMap("map");
+const user = new User();
+const company = new Company();
+
+customMap.addMarker(user);
+customMap.addMarker(company);
+
+// customMap.addUserMarker(user);
+// customMap.addCompanyMarker(company);
